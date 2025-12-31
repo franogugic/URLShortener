a837b9de4b6aa53912beb28766d0490b5e1294bb
franogugic <franogugic@icloud.com>
2025-12-31 23:01:01 +0100

    implement short URL code redirect to long URL

--- Backend/UrlShortener.API/Controllers/UrlController.cs
+++ Backend/UrlShortener.API/Controllers/UrlController.cs
@@ -1,9 +1,11 @@
 using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using UrlShortener.Application.Interfaces;
 using UrlShortener.Application.URLs.DTO_s;
 using UrlShortener.Application.URLs.Interfaces;
-using UrlShortener.Application.URLs.Services;
+using UrlShortener.Domain.Entities;
+
 
 namespace UrlShortener.API.Controllers;
 
@@ -20,6 +22,7 @@ public class UrlController : ControllerBase
         _userService = userService;
     }
     
+    [Authorize]
     [HttpPost("create")]
     public async Task<IActionResult> CreateAsync([FromBody] CreateUrlRequestDTO request, CancellationToken cancellationToken)
     {
@@ -35,7 +38,42 @@ public class UrlController : ControllerBase
         
         return Ok(response);
         
-        //ovde umj Create async getByIDurl funkciju napravit
-        //return CreatedAtAction(nameof(CreateAsync), new { id = response.Id }, response);
+    }
+
+    [Authorize]
+    [HttpGet("getAllUrlsByUserId")]
+    public async Task<IActionResult> GetAllUrlsByUser(CancellationToken cancellationToken)
+    {
+        var userId = Guid.Parse(User.FindFirst(ClaimTypes.NameIdentifier)?.Value); 
+        
+        var urls = await _urlService.GetAllUrlsByUser(userId, cancellationToken);
+        return Ok(urls);
+    }
+
+    [Authorize]
+    [HttpGet("getUrlById/{id}")]
+    public async Task<IActionResult> GetUrlById(Guid id, CancellationToken cancellationToken)
+    {
+        var currentUserId = Guid.Parse(User.FindFirst(ClaimTypes.NameIdentifier)?.Value);
+        
+        var url = await _urlService.GetUrlById(id, currentUserId ,cancellationToken);
+        return Ok(url);
+    }
+
+    [Authorize]
+    [HttpGet("delete/{id}")]
+    public async Task<IActionResult> DeleteUrl(Guid id, CancellationToken cancellationToken)
+    {
+        var currentUserId = Guid.Parse(User.FindFirst(ClaimTypes.NameIdentifier)?.Value);
+        await _urlService.DeleteAsync(id, currentUserId, cancellationToken);
+        return NoContent();
+    }
+
+    [AllowAnonymous]
+    [HttpGet("{shortUrlCode}")]
+    public async Task<IActionResult> RedirectToLongUrl(String shortUrlCode, CancellationToken cancellationToken)
+    {
+        var urlLong = await _urlService.GetLongUrlByCode(shortUrlCode, cancellationToken);
+        return Redirect(urlLong);
     }
 }
